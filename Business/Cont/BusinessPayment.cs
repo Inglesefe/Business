25c5efd704db3722ef48fb591e19cd3c7088261e
Leandro Baena Torres <leandrobaena@paradigma.com.co>
2023-09-22 08:08:14 -0500

    Ampliación cobertura en pruebas unitarias

---
+++ Business/Cont/BusinessPayment.cs
@@ -0,0 +1,19 @@
+﻿using Dal;
+using Entities.Cont;
+
+namespace Business.Cont
+{
+    /// <summary>
+    /// Realiza la persistencia de los pagos en la base de datos
+    /// </summary>
+    public class BusinessPayment : BusinessBase<Payment>
+    {
+        #region Constructors
+        /// <summary>
+        /// Inicializa la persistencia
+        /// </summary>
+        /// <param name="persistent">Persistencia en base de datos de los pagos</param>
+        public BusinessPayment(IPersistentWithLog<Payment> persistent) : base(persistent) { }
+        #endregion
+    }
+}
