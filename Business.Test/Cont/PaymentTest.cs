811c12d8f69502e7d87035031db9abee0a40eefc
Leandro Baena Torres <leandrobaena@paradigma.com.co>
2023-09-22 08:25:37 -0500

    Cambio en pruebas unitarias

---
+++ Business.Test/Cont/PaymentTest.cs
@@ -0,0 +1,24 @@
+﻿using Business.Cont;
+using Dal.Cont;
+
+namespace Business.Test.Cont
+{
+    /// <summary>
+    /// Realiza las pruebas sobre la clase de persistencia de pagos
+    /// </summary>
+    [Collection("Tests")]
+    public class PaymentTest
+    {
+        #region Methods
+        /// <summary>
+        /// Prueba la creación de una capa de negocio de un pago
+        /// </summary>
+        [Fact]
+        public void CreatePaymentTest()
+        {
+            //Act, Assert
+            Assert.IsType<BusinessPayment>(new BusinessPayment(new PersistentPayment("")));
+        }
+        #endregion
+    }
+}
